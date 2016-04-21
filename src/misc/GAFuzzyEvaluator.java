849fd24bcc8c63017987021a180712f292b690b4
adf-telkomuniv <adf.asd.telkomuniversity@gmail.com>
2016-04-21 15:18:59 +0700

    Merge origin/master
    Conflicts:
    	JavaApplication2/src/javaapplication2/JavaApplication2.java

--- src/misc/GAFuzzyEvaluator.java
+++ src/misc/GAFuzzyEvaluator.java
@@ -43,7 +43,7 @@ public class GAFuzzyEvaluator implements ChromosomeEvaluator {
     public double[][] readFile() {
         double[][] x = new double[2][2];
 //        String fileName = "data\\dataFuzzy.txt";
-        String fileName = "C:\\Users\\dee\\Documents\\GitHub\\TestLibAI\\data\\dataFuzzy.txt";
+        String fileName = "dataFuzzy.txt";
         List<String> list = new ArrayList<>();
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
 
