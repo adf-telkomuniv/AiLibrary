c0ed7dd330e9956e64e1f3821b6ca620a05c8e77
Anditya Arifianto <anditya.arifianto@gmail.com>
2016-04-27 14:37:05 +0700

    update

--- src/cnn/Trainers.java
+++ src/cnn/Trainers.java
@@ -5,17 +5,70 @@
  */
 package cnn;
 
+import java.util.Date;
+
 /**
  *
  * @author dee
  */
 public class Trainers {
-    private Vol vol;
-    public Trainers(){
+
+    private Net net;
+    private double learning_rate;
+    private double l1_decay;
+    private double l2_decay;
+    private int batch_size;
+    private String method;
+    private double momentum;
+    private double ro;
+    private double eps = 1e-8;
+    private double beta1;
+    private double beta2;
+    private int k;
+    private double[] gsum;
+    private double[] xsum;
+    private boolean regression;
+
+    public Trainers(Net net, Options opt) {
+        this.net = net;
+        learning_rate = (double) opt.getOpt("learning_rate", 0.01);
+        l1_decay = (double) opt.getOpt("l1_decay", 0.0);
+        l2_decay = (double) opt.getOpt("l2_decay", 0.0);
+        batch_size = (int) opt.getOpt("batch_size", 1);
+        method = (String) opt.getOpt("method", "sgd");
+
+        momentum = (double) opt.getOpt("momentum", 0.9);
+        ro = (double) opt.getOpt("ro", 0.95);
+        eps = (double) opt.getOpt("eps", 0.95);
+        beta1 = (double) opt.getOpt("beta1", 0.95);
+        beta2 = (double) opt.getOpt("beta2", 0.95);
+
+        k = 0;
+        if (this.net.getLayer(this.net.getLayers().size() - 1).getLayer_type().equals("regression")) {
+            regression = true;
+        } else {
+            regression = false;
+        }
+    }
+
+    public Options train(Vol x, int[] y) {
+        Date start = new Date();
+        net.forward(x, true);
+        Date end = new Date();
+        long fwd_time = end.getTime() - start.getTime();
+        System.out.println("forward time = " + (fwd_time / (1000 * 60 * 60)));
+
+        start = new Date();
+        double cost_loss = net.backward(y);
+        double l2_decay_loss = 0.0;
+        double l1_decay_loss = 0.0;
+        end = new Date();
+        long bwd_time = end.getTime() - start.getTime();
+        System.out.println("backward time = " + (bwd_time / (1000 * 60 * 60)));
+
+        //check if regression
         
+        return null;
     }
-    
-    public Trainers(Vol vol){
-        this.vol = vol;
-    }
+
 }
