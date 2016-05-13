54f29d0717437700b353dd88150c7aa8d8393456
Anditya Arifianto <anditya.arifianto@gmail.com>
2016-05-13 14:41:22 +0700

    update

--- src/cnn/MagicNet.java
+++ src/cnn/MagicNet.java
@@ -10,5 +10,29 @@ package cnn;
  * @author dee
  */
 public class MagicNet {
-    
+
+    private double[] data;
+    private double[] labels;
+    private double train_ratio;
+    private int num_folds;
+    private int num_candidates;
+    private int num_epochs;
+    private int ensemble_size;
+
+    public MagicNet(double[] data, double[] labels, Options opt) {
+        if (opt == null) {
+            opt = new Options();
+        }
+        this.data = data;
+        this.labels = labels;
+        train_ratio = (double) opt.getOpt("train_ratio", 0.7);
+
+        num_folds = (int) opt.getOpt("num_folds", 10);
+        num_candidates = (int) opt.getOpt("num_candidates", 50);
+
+        num_epochs = (int) opt.getOpt("num_epochs", 50);
+        ensemble_size = (int) opt.getOpt("ensemble_size", 10);
+        
+        
+    }
 }
